3e070b777c48d2f9c7056a3977e464c223342bc4
Danstiv <50794055+Danstiv@users.noreply.github.com>
2024-08-07 00:16:57 +0700

    Add logging to process_master_changes script

--- scripts/process_master_changes.py
+++ scripts/process_master_changes.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 import subprocess
 from pathlib import Path
@@ -15,14 +16,23 @@ from utils import (
 REPOSITORY_DIR = Path(".")
 PROJECTS_DIR = Path(os.environ["PROJECTS_DIR"])
 
+logger = logging.getLogger("process_master_changes")
+logger.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+logger.addHandler(console_handler)
+
 
 def process_project(project_path):
+    logger.info(f"Processing project {project_path}")
     convert_resx_files(project_path)
     generate_pot_file(project_path)
     process_docs(project_path)
+    logger.info("Project processed")
 
 
 def convert_resx_files(project_path):
+    logger.info("Processing resx files")
     english_files = []
     has_changes = False
     for file in project_path.glob("*.resx"):
@@ -30,7 +40,9 @@ def convert_resx_files(project_path):
             english_files.append(file)
             if not has_changes and file_updated(file):
                 has_changes = True
+    logger.info(f"Found {len(english_files)} english resx files")
     if not has_changes:
+        logger.info("No changed english resx files found")
         return
     lng = {"Culture": "en", "Language": "english"}
     for file in english_files:
@@ -38,10 +50,13 @@ def convert_resx_files(project_path):
     lng = json.dumps(lng, indent=2, ensure_ascii=False, sort_keys=True)
     with (project_path / "english.lng").open("w", encoding="utf-8", newline="") as f:
         f.write(lng)
+    logger.info("Resx files processed")
 
 
 def parse_resx(file):
+    logger.info(f"Parsing resx file {file}")
     namespace, _ = parse_resx_filename(file.name)
+    logger.info(f"File namespace is {namespace}")
     with file.open("r", encoding="utf-8") as f:
         root = ElementTree.fromstring(f.read())
     lng = {}
@@ -50,12 +65,15 @@ def parse_resx(file):
         text = data.find("value").text
         text = convert_braces_to_percents(text)
         lng[f"{namespace}_{name}"] = text
+    logger.info("Resx file parsed")
     return lng
 
 
 def generate_pot_file(project_path):
+    logger.info("Generating pot file from english lng file")
     english_file = project_path / "english.lng"
     if not file_updated(english_file):
+        logger.info("File is not changed")
         return
     with english_file.open("r") as f:
         lng = json.load(f)
@@ -71,6 +89,7 @@ def generate_pot_file(project_path):
         "w", encoding="utf-8", newline=""
     ) as f:
         f.write(pot)
+    logger.info("Pot file generated")
 
 
 def get_source_line_for_pot(string):
@@ -85,6 +104,7 @@ def get_source_line_for_pot(string):
 
 
 def generate_pot_file_from_source(source, package_name):
+    logger.info("Generating pot file from source")
     process = subprocess.Popen(
         [
             "xgettext",
@@ -108,23 +128,30 @@ def generate_pot_file_from_source(source, package_name):
         pot += process.stdout.read()
     if process.returncode != 0:
         raise RuntimeError(f"xgettext failed with code {process.returncode}")
+    logger.info("Pot generated from source")
     return pot.decode()
 
 
 def process_docs(project_path):
+    logger.info("Processing project docs")
     docs_path = project_path / "docs"
     if not docs_path.is_dir():
+        logger.info("No docs in project")
         return
     en_docs_path = docs_path / "en"
     process_en_docs(en_docs_path)
+    logger.info("Project docs processed")
 
 
 def process_en_docs(en_docs_path):
+    logger.info(f"Processing english docs: {en_docs_path}")
     for md_file in en_docs_path.glob("*.md"):
         process_docs_md_file(md_file)
+    logger.info("English docs processed")
 
 
 def process_docs_md_file(md_file):
+    logger.info(f"Processing docs md file {md_file}")
     package_name = md_file.relative_to(REPOSITORY_DIR).parts[0]
     process = subprocess.Popen(
         [
@@ -167,6 +194,7 @@ def process_docs_md_file(md_file):
     pot_file = md_file.with_suffix(".pot")
     with pot_file.open("wb") as f:
         f.write(pot)
+    logger.info("Md file processed")
 
 
 def main():
